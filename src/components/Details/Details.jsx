548c3788eae4b75501d07a41d77a32a5659655b8
AJ Timek <Timek612@gmail.com>
2022-08-27 21:46:32 -0500

    more details page setup

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,13 +1,25 @@
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 
 function Details () {
     const movie = useSelector(store => store.movieSetter);
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push('/')
+
+
+    }
 
     return (
         <>
-        <h1>placehodler</h1>
+        <button onClick={handleClick}>Back</button>
+        <h1>{movie.title}</h1>
         <img src={movie.poster} alt="" />
+        <div className='descriptionBox'>
+        <p>{movie.description}</p>
+        </div>
         </>
     )
 }
